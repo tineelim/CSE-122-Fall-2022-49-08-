43de2528dfa1c6e2dd7f030298fff0d58161809a
Tasabil Islam Mojumder <109529082+tineelim@users.noreply.github.com>
2022-07-23 11:46:43 +0600

    Add files via upload

---
+++ Lab 01/3.353_49-08.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+int n, P, V, T, c=0;
+
+cin>>n;
+
+while(n--) {
+
+    cin >> P >> V >> T;
+
+    if(P + V + T >= 2)
+    {
+        c+=1;
+    }
+}
+cout << c << endl;
+}
