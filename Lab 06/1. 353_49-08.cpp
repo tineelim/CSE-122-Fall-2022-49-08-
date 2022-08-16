284dec18d0ee7f13269562c04ec702063e90cb61
Tasabil Islam Mojumder <109529082+tineelim@users.noreply.github.com>
2022-08-16 16:27:48 +0600

    Create 1. 353_49-08.cpp

---
+++ Lab 06/1. 353_49-08.cpp
@@ -0,0 +1,19 @@
+//Problem 617A - Elephant
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+
+    if(x%5 == 0){
+        cout<<x/5<<endl;
+    }
+    else
+    {
+        cout<<x/5+1<<endl;
+    }
+
+}
