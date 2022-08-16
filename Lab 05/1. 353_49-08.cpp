4dc01a8b2c51fd4b6572815c32eaf1672e5eb15f
Tasabil Islam Mojumder <109529082+tineelim@users.noreply.github.com>
2022-08-16 11:58:46 +0600

    Add files via upload

---
+++ Lab 05/1. 353_49-08.cpp
@@ -0,0 +1,14 @@
+// Problem : 266A - Stones on the Table
+#include <iostream>
+using namespace std;
+string b;
+int a, c;
+int main() {
+    cin >> a >> b;
+    for (int i = 0; i < a; i++) {
+        if (b[i] == b[i + 1]) {
+            c++;
+        }
+    }
+    cout << c;
+}
