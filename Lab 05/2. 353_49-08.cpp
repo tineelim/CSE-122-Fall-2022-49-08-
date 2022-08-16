11149725899bfae89c7d70acd29025cd69c68114
Tasabil Islam Mojumder <109529082+tineelim@users.noreply.github.com>
2022-08-16 11:59:13 +0600

    Add files via upload

---
+++ Lab 05/2. 353_49-08.cpp
@@ -0,0 +1,19 @@
+// Problem : 791A - Bear and Big Brother
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int a,b,c=0;
+    cin>>a>>b;
+
+        while(a<=b)
+        {
+            a=a*3;
+            b=b*2;
+            c++;
+        }
+    cout<<c<<endl;
+    return 0;
+}
---
+++ Lab 05/3. 353_49-08.cpp
@@ -0,0 +1,19 @@
+//Problem 546A – Soldier and Bananas
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k, n, w;
+    cin >> k >> n >> w;
+    int sum = 0;
+    for(int i = 1; i <= w; i++)
+    {
+        sum += i;
+    }
+    int d = (sum * k) - n;
+    if(d < 0)
+        cout << "0";
+    else
+        cout << d;
+
+}
